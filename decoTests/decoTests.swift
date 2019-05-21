ac0e985a69a4826d6a4bff42952fb36c2497f4b4
Souksouvanh Thomas <souksouvanh.thomas@gmail.com>
2019-05-21 09:54:05 +0200

    - Add travis

--- decoTests/decoTests.swift
+++ decoTests/decoTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class decoTests: XCTestCase {
 
+    let converter = Converter()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,16 +21,8 @@ class decoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testConversionForOne() {
+        let result = converter.convert(2)
+        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
